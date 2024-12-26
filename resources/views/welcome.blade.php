c8d85acafe04bb22e249195c5f702c398e3575ce
Vlado Bosnjak <vlado.bosnjak@elevatebits.com>
2024-12-27 00:36:26 +0100

    created some basic layout of blade templates. added navbar. closing basic setup with this. working on fleet editor next.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Battlefleet Gothic Fleetlist Generator</title>
-
-        <link rel="icon" type="image/x-icon" href="{{ asset("images/favicon.png") }}">
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        @vite('resources/css/app.css')
-    </head>
-    <body id="main-body">
-        <header id="bfg-header">
-            <div class="banner-container">
-                <a id="logo-home" href="{{ route('home') }}">
-                    <img id="bfg-logo" src="{{ asset("images/bfg-logo.png") }}" alt="bfg logo">
-                </a>
-            </div>
-        </header>
-        <div class="quick-get-started">
-            <h1>Build your fleet!</h1>
-            <!-- TODO: use gw2skills landing page for reference -->
-        </div>
+@section('content')
+    <div class="quick-get-started">
+        <h1>Build your fleet!</h1>
+        <!-- TODO: use gw2skills landing page for reference -->
+    </div>
     <div class="explore-fleets">
         <h1>Explore fleets</h1>
         <div class="most-popular">
             <h2>Most popular fleets</h2>
         </div>
     </div>
-    </body>
-</html>
+@endsection
