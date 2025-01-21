a944cc1ed18266bf76b8aca80a89fd6317efe368
Vlado Bosnjak <vlado.bosnjak@elevatebits.com>
2025-01-21 22:02:29 +0100

    Forgot to add custom FormRequest

---
+++ app/Http/Requests/FleetEditorFormRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FleetEditorFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+//    public function authorize(): bool
+//    {
+//        return false;
+//    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $step = $this->input('step');
+        $this->validateStep($step);
+
+        $rules = [];
+
+        switch ($step) {
+            case 'faction':
+                $rules = [
+                    'faction' => 'required|string|max:25',
+                ];
+                break;
+            case 'fleet-list':
+                $rules = [
+
+                ];
+                break;
+            case 'fleet':
+                $rules = [
+
+                ];
+                break;
+            default:
+        }
+
+        return $rules;
+    }
+
+    private function validateStep($step)
+    {
+        if (!$step || !in_array($step, ['faction', 'fleet-list', 'fleet'])) {
+            abort(422, 'Invalid step provided');
+        }
+    }
+}
