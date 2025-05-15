cdff0ae692078e5aa94757acd6584787eca1e747
Vlado Bosnjak <vlado.bosnjak@elevatebits.com>
2025-05-15 19:51:30 +0200

    Extended refits table with more columns to better represent different refits. Started reworking RefitsSeeder.php to accommodate db changes

--- database/migrations/2025_02_11_122322_create_refits_table.php
+++ database/migrations/2025_02_11_122322_create_refits_table.php
@@ -16,7 +16,12 @@ return new class extends Migration
             $table->string('name', 50);
             $table->string('text', 75);
             $table->text('text_long')->nullable();
-            $table->string('module', 255);
+            $table->string('module1', 50);
+            $table->string('action1', 50)->nullable();
+            $table->string('value1', 50)->nullable();
+            $table->string('module2', 50)->nullable();
+            $table->string('action2', 50)->nullable();
+            $table->string('value2', 50)->nullable();
         });
     }
 
