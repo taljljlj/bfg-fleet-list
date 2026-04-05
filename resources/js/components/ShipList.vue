b5e96330009ab974c4e504b7104a4a2b31eed24f
Vlado Bosnjak <vlado.bosnjak@elevatebits.com>
2026-04-06 01:43:25 +0200

    Converted ShipList.vue to tailwind. Added ABOUT section to README.md

--- resources/js/components/ShipList.vue
+++ resources/js/components/ShipList.vue
@@ -28,28 +28,34 @@ const toggleGroup = (event) => {
             <li
                 v-for="(shipGroup, shipType) in shipList"
                 :key="shipType"
-                class="ship-type-group collapsed"
+                class="ship-type-group group collapsed text-white py-1"
             >
                 <h4
-                    class="ship-type-group-title"
+                    class="ship-type-group-title ship-list-separator relative flex items-center justify-end gap-2.5 pb-1 text-right text-secondary cursor-pointer text-lg tracking-wide "
                     @click="toggleGroup"
                 >
-                    {{ shipType }}s
+                    <span>{{ shipType }}s</span>
                     <span class="caret-icon">
-                        <img :src="caretIcon" alt="caret-icon">
+                        <img :src="caretIcon" alt="caret-icon" class="h-6 opacity-70 rotate-180 group-[.collapsed]:rotate-0">
                     </span>
                 </h4>
-                <ul class="ship-type-container font-secondary tracking-wide">
-                    <li v-for="ship in shipGroup" :key="ship.id">
-                        <span class="ship-class">
+                <ul class="ship-type-container ship-list-separator relative group-[.collapsed]:hidden">
+                    <li
+                        v-for="ship in shipGroup"
+                        :key="ship.id"
+                        class="text-white py-1 font-family-secondary tracking-wide align-middle flex justify-between"
+                    >
+                        <span class="ship-class grow text-left">
                           {{ ship.class }}{{ ship.type === 'Escort' ? ' Squadron' : '' }}
                         </span>
-                        <span class="ship-pts font-secondary tracking-tight">{{ ship.points }}</span>
+                        <span class="ship-pts font-secondary tracking-tight mx-2.5">
+                            {{ ship.points }}
+                        </span>
                         <span
-                            class="ship-add-btn"
+                            class="ship-add-btn text-right cursor-pointer opacity-70 hover:opacity-100 user-select-none hover:filter-[drop-shadow(0_0_10px_#c8c5dc)_hue-rotate(45deg)]"
                             @click="handleShipAdd(ship.id)"
                         >
-                            <img :src="addShipIcon" alt="Add Ship Icon">
+                            <img :src="addShipIcon" alt="Add Ship Icon" class="h-6 w-6">
                         </span>
                     </li>
                 </ul>
@@ -57,93 +63,3 @@ const toggleGroup = (event) => {
         </ul>
     </div>
 </template>
-
-<style scoped>
-.ship-list ul {
-    list-style: none;
-    padding: 0;
-}
-
-.ship-list li {
-    color: white;
-    padding: 5px 0;
-}
-
-.ship-class {
-    flex-grow: 1;
-    text-align: left;
-}
-
-.ship-add-btn,
-.ship-pts {
-    text-align: right;
-}
-
-.ship-pts {
-    margin: 0 10px;
-}
-
-.ship-add-btn {
-    height: 19px;
-    cursor: pointer;
-    opacity: 0.7;
-    user-select: none;
-}
-
-.ship-add-btn:hover {
-    opacity: 1;
-    filter: drop-shadow(0 0 10px #c8c5dc) hue-rotate(45deg);
-}
-
-.ship-type-group-title {
-    position: relative;
-    text-align: right;
-    padding-bottom: 5px;
-    color: #c8c5dc;
-    cursor: pointer;
-    margin: 0;
-    user-select: none;
-    font-size: 18px;
-}
-
-.caret-icon {
-    vertical-align: sub;
-    padding-left: 10px;
-}
-
-.caret-icon img {
-    height: 22px;
-    opacity: 0.7;
-    rotate: 180deg;
-}
-
-.ship-type-group-title::after,
-.ship-type-container:after {
-    position: absolute;
-    display: block;
-    content: "";
-    bottom: 0;
-    right: -25px;
-    height: 2px;
-    width: 250px;
-    background: linear-gradient(to right, transparent 0%, #c8c5dc 100%);
-}
-
-.ship-type-container {
-    position: relative;
-}
-
-.ship-type-group.collapsed .ship-type-container {
-    height: 0;
-    overflow-y: hidden;
-}
-
-.ship-type-group.collapsed .caret-icon img {
-    rotate: 0deg;
-}
-
-.ship-type-container li {
-    display: flex;
-    justify-content: space-between;
-}
-</style>
