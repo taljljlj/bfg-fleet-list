103cc8a86e1d8e282e8501f2ce5a5449e216cd2a
Vlado Bosnjak <putty.destroyer.69@hotmail.com>
2026-07-02 00:36:01 +0200

    Added icons instead of text to fleet action buttons

--- resources/js/components/controls/FleetActions.vue
+++ resources/js/components/controls/FleetActions.vue
@@ -9,9 +9,21 @@ defineProps({
 </script>
 
 <template>
-    <div class="fleet-actions section-subsection flex flex-row justify-evenly my-6">
-        <button @click="onExportPdf" class="export-btn btn-primary">Export PDF</button>
-        <button id="exportUrl" class="export-btn btn-primary">Share URL</button>
-        <button id="exportStore" class="export-btn btn-primary">Save</button>
+    <div class="fleet-actions section-subsection flex flex-row justify-evenly pt-2.5">
+        <button @click="onExportPdf" class="export-btn btn-primary">
+            <span class="hover:opacity-80">
+                <img src="/images/fleet-builder/pdf-export-icon.png" alt="PDF Export">
+            </span>
+        </button>
+        <button id="exportUrl" class="export-btn btn-primary">
+            <span class="hover:opacity-80">
+                <img src="/images/fleet-builder/share-icon.png" alt="Share">
+            </span>
+        </button>
+        <button id="exportStore" class="export-btn btn-primary">
+            <span class="hover:opacity-80">
+                <img src="/images/fleet-builder/save-icon.png" alt="Save">
+            </span>
+        </button>
     </div>
 </template>
