830d18fe202108a4465b260370f220bfb47b6b0e
Vlado Bosnjak <putty.destroyer.69@hotmail.com>
2026-07-04 22:34:39 +0200

    Fixed pdf export. Issue was local dev env and puppeteer cache path being stubborn on Windows. Best approach to fixing it is copying puppeteer cache from download path to read path as system env vars dont seem to do anything

--- resources/js/components/controls/FleetActions.vue
+++ resources/js/components/controls/FleetActions.vue
@@ -3,27 +3,24 @@ defineProps({
     fleetId: Number,
     onExportPdf: Function
 })
-
-// TODO: add icons to buttons instead of text
-// TODO: fix export pdf, currently throwing error
 </script>
 
 <template>
     <div class="fleet-actions section-subsection flex flex-row justify-evenly pt-2.5">
         <button @click="onExportPdf" class="export-btn btn-primary">
-            <span class="hover:opacity-80">
+            <div class="hover:opacity-80 p-2">
                 <img src="/images/fleet-builder/pdf-export-icon.png" alt="PDF Export">
-            </span>
+            </div>
         </button>
         <button id="exportUrl" class="export-btn btn-primary">
-            <span class="hover:opacity-80">
+            <div class="hover:opacity-80 p-2">
                 <img src="/images/fleet-builder/share-icon.png" alt="Share">
-            </span>
+            </div>
         </button>
         <button id="exportStore" class="export-btn btn-primary">
-            <span class="hover:opacity-80">
+            <div class="hover:opacity-80 p-2">
                 <img src="/images/fleet-builder/save-icon.png" alt="Save">
-            </span>
+            </div>
         </button>
     </div>
 </template>
